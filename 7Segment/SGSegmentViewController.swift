56b115165dd3b304a41e79f15361a0e208c0945f
Adi <mathewa6@users.noreply.github.com>
2016-10-23 16:01:08 -0400

    Added SGSegmentViewController and it's xib which are then thrown into Main.storyboard.

---
+++ 7Segment/SGSegmentViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SGSegmentViewController.swift
+//  7Segment
+//
+//  Created by Adi Mathew on 10/23/16.
+//  Copyright © 2016 RCPD. All rights reserved.
+//
+
+import UIKit
+
+class SGSegmentViewController: UIViewController {
+
+    @IBOutlet var segments: [SGSegmentView]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
