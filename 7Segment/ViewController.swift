95d6dfd826e49b339e0971749db71d2aaa80e894
Adi <mathewa6@users.noreply.github.com>
2016-12-22 14:26:21 -0500

    Added UIPickerView to ViewController to debug.

--- 7Segment/ViewController.swift
+++ 7Segment/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet var valuePicker: UIPickerView!
+    
+    let pickerValues: [UInt8] = Array(0 ..< 16)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.valuePicker.delegate = self
+        self.valuePicker.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - UIPickerView methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerValues[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(pickerValues[row])
+    }
 }
 
